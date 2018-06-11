87703f15d8aebce86475f96df56dbe7b1802a350
Vinicius <vinicius.alves@itarget.com>
2018-06-11 17:12:05 -0300

    Mudança do nome da página

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>LISTA DE CADASTRO</title>
+	<title>CADASTRO</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<script src="js/jquery.js"></script>
 	<script src="js/bootstrap.min.js"></script>
@@ -72,7 +72,7 @@
 		}
 	</script>
 <body>
-	<hr><h1>Lista de Cadastro</h1><hr>
+	<hr><h1>Página de Cadastro</h1><hr>
 	<br/>
 		<p class="container-fluid">Insira seus dados</p>
 	<form name="form_cadastro" action="insert.php" method="POST" class="container-fluid" onsubmit="return salvar()">
@@ -81,7 +81,7 @@
 		</div>
 		<div class="form-group">
 				<input type="tel" name="num_pessoa" size="21" maxlength="9" id="num_pessoa" placeholder="Contato">
-		3</div>
+		</div>
 		<div class="form-group">
 				<input type="text" name="end_pessoa" size="21"  id="end_pessoa" placeholder="Endereço">
 		</div>
